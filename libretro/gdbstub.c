6c1e047eb1f67b7c83c326236aa4f626f9f2934c
Colin Snover <github.com@zetafleet.com>
2024-07-27 23:19:03 -0500

    Pass correct information to GDB on startup

--- libretro/gdbstub.c
+++ libretro/gdbstub.c
@@ -8,10 +8,13 @@
  *  Modified 19981104 by John Marshall  <jmarshall@acm.org>
  */
 
+#include <stdint.h>
 #include <PalmTypes.h>
 #include <DebugMgr.h>
 #include <FeatureMgr.h>
 #include <SystemMgr.h>
+
+extern uint8_t _sdata, _sbss;
 
 static void StartDebug(UInt16 flags)
 {
@@ -20,8 +23,26 @@ static void StartDebug(UInt16 flags)
 
     UInt32 feature = 0;
     FtrGet('gdbS', 0, &feature);
-    if (feature == 0x12BEEF34)
-        DbgSrcBreak();
+    if (feature != 0x12BEEF34)
+        return;
+
+    // Tell the debugger the location of .text (d0), .bss (d1), .data (d2), and
+    // PilotMain (a0), and call DbgBreak (trap 8). These registers are used by
+    // the custom PalmOS GCC wire protocol. There may be another one.
+    __asm__(
+        "\tlea     %0, %%a0\n"
+        "\tmove.l  %%a0, %%d2\n"
+        "\tlea     %1, %%a0\n"
+        "\tmove.l  %%a0, %%d1\n"
+        "\tlea     _start(%%pc), %%a0\n"
+        "\tmove.l  %%a0, %%d0\n"
+        "\tsub.l   #_start, %%d0\n"
+        "\tlea     PilotMain(%%pc), %%a0\n"
+        "\tmove.l  #0x12BEEF34, %%d3\n"
+        "\ttrap    #8"
+        :
+        : "g" (_sdata), "g" (_sbss)
+        : "d0", "d1", "d2", "d3", "a0");
 }
 
 static void *hook __attribute__ ((section ("preinit"), unused)) = StartDebug;
